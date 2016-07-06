693a13efe0fbc39abab37b62168a3e7c6cfa851b
Hom, Kenneth <khom@admins-macbook-air.local>
2016-07-06 10:38:40 -0400

    Initial Commit

---
+++ FaceIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Hom, Kenneth on 7/6/16.
+//  Copyright © 2016 Hom, Kenneth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
